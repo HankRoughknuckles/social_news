a3b1e38481f0ef08d6946ef215c6e1bb728dba32
Thomas Morris <thomas.imorris@gmail.com>
2014-10-28 11:58:12 +0200

    Make page object for index view

--- spec/support/page_objects/links_index_page.rb
+++ spec/support/page_objects/links_index_page.rb
@@ -2,6 +2,8 @@ class LinksIndexPage
   include Capybara::DSL
 
   def initialize
+    @add_link_button = "a#add_link"
+    @links_list = ".links"
   end
 
   def has_title?(passed_title)
@@ -9,21 +11,35 @@ class LinksIndexPage
   end
 
   def has_an_add_link_button?
-    add_link_button
+    has_css? @add_link_button
   end
 
   def has_link_for?(link)
-    links_list.has_css? "tr", text: link.link
+    has_css? "#{@links_list} a", text: link.address
   end
 
-
-  private
-
-  def add_link_button
-    find 'a#add_link'
+  def has_edit_button_for?(link)
+    has_css? "#{@links_list} #edit-#{link.id}"
   end
 
-  def links_list
-    find '.links'
+  def has_delete_button_for?(link)
+    has_css? "#{@links_list} #delete-#{link.id}"
+  end
+
+  def click_add_link_button
+    find( @add_link_button ).click
+  end
+
+  def click_save_button
+    page.find(:css, "input[type=submit]").click
+  end
+
+  def create_link(link)
+    click_add_link_button
+
+    fill_in 'link_address', with: link.address
+    fill_in 'link_description', with: link.description
+
+    click_save_button
   end
 end
