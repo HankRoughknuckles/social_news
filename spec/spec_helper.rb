3aad0e897862855847aac2926914c4220db812d6
Thomas Morris <thomas.imorris@gmail.com>
2014-10-29 09:45:27 +0200

    Remove view spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,8 +30,6 @@ Spork.prefork do
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
   RSpec.configure do |config|
-    config.include Devise::TestHelpers, type: :view
-
     # ## Mock Framework
     #
     # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
